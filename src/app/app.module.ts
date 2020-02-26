c489c90bd5922f04d0b60428be31701f721610c7
Marin Balabanov <marincomics@gmail.com>
2020-02-26 10:51:24 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+
+export const products = [
+	{
+ 		name: 'Dog Food',
+		price: 4.99,
+		photo: 'assets/img/dog-food.jpg',
+		description: 'This is beed wet food. Science Diet Canine Senior was developed to meet the nutrient and energy needs of mature adult dogs 7+ years of age.'
+	},
+ 	{
+		name: 'Cat Food',
+		price: 3.99,
+		photo: 'assets/img/cat-food.jpg',
+		description: 'Elevate your cat\'s meals to the next level by offering her a bowl of Purina Fancy Feast Classic Pate Chicken Feast wet cat food. Each delicious serving is packed full of high-quality ingredients to create a 100% complete and balanced dish that provides all the nourishment your cat needs to keep her healthy and happy.'
+	},
+	{
+		name: 'Hamster Food',
+		price: 2.99,
+		photo: 'assets/img/hamster-food.jpg',
+		description: 'Oxbow Essentials Healthy Handfuls Hamster/Gerbil Food is the cornerstone of a complete, balanced diet you can feel good about serving your pet. From ideal weight and better digestion to dental health and more, this food promotes better and lasting health across the board.'
+	},
+	{
+		name: 'Fish Food',
+		price: 1.99,
+		photo: 'assets/img/fish-food.jpg',
+		description: 'TetraFin Goldfish Flakes features a New and improved formula for cleaner and clearer water- guaranteed . This scientifically developed mix of highly nutritious ingredients plus vitamins, minerals and trace elements is easily digested.'
+	}
+];
